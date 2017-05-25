82e6e556d2a0dba0b48de4bfe774e6ba19c1d674
jiheu <jiheu@DESKTOP-DE5CV3N>
2017-05-25 11:44:46 +0900

    실습과제 13 제출

---
+++ JavaPractice/src/org/dimigo/interfaces/DBTest.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package org.dimigo.interfaces;
+
+/**
+ * <pre>
+ * org.dimigo.interfaces
+ * 	| _ DBTest
+ *
+ * 1. 개요 : 
+ * 2. 작성일 : 2017. 5. 25.
+ * </pre>
+ *
+ * @author jiheu
+ * @version : 1.0
+ */
+public class DBTest {
+	private static void crud(IDBManager db){
+		db.insert();
+		db.search();
+		db.update();
+		db.delete();
+		System.out.println();
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("<< 변경 전 >>");
+		crud(IDBManager.getDBObject("SYBASE"));
+		System.out.println("<< 변경 후 >>");
+		crud(IDBManager.getDBObject("ORACLE"));
+	}
+
+}
